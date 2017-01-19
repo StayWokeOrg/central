9a15aed9c783aae2922f0160f859e0703f9a1d1c
Matt Stauffer <matt@tighten.co>
2017-01-19 16:29:56 -0500

    Change topics column to string for rds' sake

--- database/migrations/2017_01_19_192927_add_topics_to_contacts_table.php
+++ database/migrations/2017_01_19_192927_add_topics_to_contacts_table.php
@@ -14,7 +14,7 @@ class AddTopicsToContactsTable extends Migration
     public function up()
     {
         Schema::table('contacts', function (Blueprint $table) {
-            $table->json('topics')->nullable();
+            $table->string('topics')->nullable();
         });
     }
 
